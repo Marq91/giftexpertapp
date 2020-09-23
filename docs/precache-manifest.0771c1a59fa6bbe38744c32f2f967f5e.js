20fd08d45f3287a2ce816f0e24ac4a08ca05a334
anibal-marquez <anibal.marquez@almacenguru.com>
2020-09-23 02:12:06 -0300

    Docs added

---
+++ docs/precache-manifest.0771c1a59fa6bbe38744c32f2f967f5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1517fc3d1ffa9b1e3ab247ec139ccba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45aab31fed238fe102f7",
+    "url": "/static/css/main.824f5e09.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45aab31fed238fe102f7",
+    "url": "/static/js/main.7e11db7f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
